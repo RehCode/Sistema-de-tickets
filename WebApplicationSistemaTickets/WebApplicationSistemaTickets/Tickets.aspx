988e0a5747ed07812e6aa6fd63e81ef98866d758
René Hernández <rene.rhc@gmail.com>
2018-05-19 13:33:34 -0600

    Crear nueva pagina aparte para mostrar tickets

---
+++ WebApplicationSistemaTickets/WebApplicationSistemaTickets/Tickets.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Tickets.aspx.cs" Inherits="WebApplicationSistemaTickets.Tickets" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolderBody" runat="server">
+    <div class="container">
+        <div class="page-header">
+            <h1>Tabla de Tickets</h1>
+        </div>
+        <p class="lead">
+            Mostrar:
+            <asp:Button ID="ButtonTodos" runat="server" Text="Todos" OnClick="ButtonTodos_Click" />
+            Por responsable:
+            <asp:DropDownList runat="server">
+                <asp:ListItem Text="Luis" />
+                <asp:ListItem Text="Alejandra" />
+            </asp:DropDownList>
+        </p>
+        <asp:GridView ID="GridViewTickets" runat="server" AllowPaging="True" OnPageIndexChanging="GridViewTickets_PageIndexChanging" PageSize="15">
+        </asp:GridView>
+    </div>
+
+</asp:Content>
---
+++ WebApplicationSistemaTickets/WebApplicationSistemaTickets/Tickets.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplicationSistemaTickets
+{
+    public partial class Tickets : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonTodos_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conexionDB = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString))
+            {
+                string query = "SELECT top (200) [ticket_id] as ID"
+                            + " ,[titulo]"
+                            + " ,[descripcion]"
+                            + " ,[fecha]"
+                            + " ,[departamentos].[departamento]"
+                            + " ,[nombre_interesado]"
+                            + " ,[categorias].[categoria]"
+                            + " ,[responsables].[nombre] as responsable"
+                            + "  FROM ((tickets"
+                            + "  inner join departamentos on tickets.departamento_id = departamentos.departamento_id)"
+                            + "  inner join categorias on tickets.categoria_id = categorias.categoria_id"
+                            + "  ) inner join responsables on tickets.responsable_id = responsables.responsable_id"
+                            + "  where solucionado=1"
+                            + "  order by fecha asc";
+                SqlCommand cmd = new SqlCommand(query);
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = conexionDB;
+                conexionDB.Open();
+
+                SqlDataAdapter adaptador = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adaptador.Fill(ds);
+                GridViewTickets.AutoGenerateColumns = true;
+                GridViewTickets.DataSource = ds.Tables[0];
+                GridViewTickets.DataBind();
+            }
+        }
+
+
+        protected void GridViewTickets_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridViewTickets.PageIndex = e.NewPageIndex;
+            GridViewTickets.DataBind();
+        }
+    }
+}
