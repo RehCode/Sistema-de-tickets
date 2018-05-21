93985fc599866f5898a43e9dbbd48ea34339551b
René Hernández <rene.rhc@gmail.com>
2018-05-21 13:11:00 -0600

    funcionando reporte de departamentos

--- WebApplicationSistemaTickets/WebApplicationSistemaTickets/reporteDepartamentos.aspx.cs
+++ WebApplicationSistemaTickets/WebApplicationSistemaTickets/reporteDepartamentos.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.UI.DataVisualization.Charting;
 
 namespace WebApplicationSistemaTickets
 {
@@ -11,7 +15,52 @@ namespace WebApplicationSistemaTickets
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                cargaFrecuencias();
+            }
+        }
 
+        protected void cargaFrecuencias()
+        {
+            int numMes = Convert.ToInt32(DropDownListMes.SelectedValue);
+            using (SqlConnection conexionDB = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString))
+            {
+
+                SqlCommand cmd = new SqlCommand("estadisticasDepartamentos", conexionDB);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@numMes", numMes);
+                conexionDB.Open();
+                cmd.ExecuteNonQuery();
+
+                SqlDataAdapter adaptador = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adaptador.Fill(ds);
+                DataTable tbTotal = ds.Tables[0];
+                DataTable tbMes = ds.Tables[1];
+
+                ChartTotal.DataSource = tbTotal;
+                ChartTotal.Series[0].XValueMember = "departamento";
+                ChartTotal.Series[0].YValueMembers = "cantidad";
+                ChartTotal.ChartAreas[0].AxisX.LabelStyle.Angle = -90;
+                ChartTotal.ChartAreas[0].AxisX.LabelStyle.Interval = 1;
+                ChartTotal.ChartAreas[0].AxisY.Title = "Numero de tickets";
+                ChartTotal.DataBind();
+
+                ChartTotalMes.DataSource = tbMes;
+                ChartTotalMes.Series[0].XValueMember = "departamento";
+                ChartTotalMes.Series[0].YValueMembers = "cantidad";
+                ChartTotalMes.ChartAreas[0].AxisX.LabelStyle.Angle = -90;
+                ChartTotalMes.ChartAreas[0].AxisX.LabelStyle.Interval = 1;
+                ChartTotalMes.ChartAreas[0].AxisY.Title = "Numero de tickets";
+                ChartTotalMes.DataBind();
+
+            }
+        }
+
+        protected void ButtonMotrarMes_Click(object sender, EventArgs e)
+        {
+            cargaFrecuencias();
         }
     }
 }
